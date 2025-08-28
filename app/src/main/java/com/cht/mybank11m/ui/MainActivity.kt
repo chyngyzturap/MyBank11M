7918ce4c4fb2480d4ddc242c9af0a751a09e50de
chyngyzturap <chyngyzturapov@gmail.com>
2025-08-28 19:45:57 +0600

    Revert "lesson6(feat): change text add account button"
    This reverts commit cfc90246298587aa40b079ed3ee1955c6993edaa.

--- app/src/main/java/com/cht/mybank11m/ui/MainActivity.kt
+++ app/src/main/java/com/cht/mybank11m/ui/MainActivity.kt
@@ -78,7 +78,7 @@ class MainActivity : AppCompatActivity() {
             AlertDialog.Builder(this@MainActivity)
                 .setTitle("Добавить счет")
                 .setView(this)
-                .setPositiveButton("Создать счет"){_,_, ->
+                .setPositiveButton("Добавить"){_,_, ->
                     val account = Account(
                         name = nameInput.text.toString(),
                         balance = balanceInput.text.toString().toInt(),
